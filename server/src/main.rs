23bcec52e5bfc7add3492782e001091e062ade1b
13r0ck <bnr@tuta.io>
2021-05-10 10:38:45 -0600

    Bug: Fix SPA routing on server + Improve Dockerfile build speed

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-use rocket_contrib::serve::StaticFiles;
-use rocket::response::Redirect;
+#[macro_use]
+extern crate rocket;
+use rocket::response::NamedFile;
+use std::path::{Path, PathBuf};
 
 #[get("/")]
-fn index() -> Redirect {
-    Redirect::to("/index.html")
+fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("server/public/index.html")).ok()
 }
 
+#[get("/<file..>", rank = 10)]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("server/public/").join(file))
+        .ok()
+        .or_else(|| NamedFile::open(Path::new("server/public/index.html")).ok())
+}
 
 fn main() {
-    rocket::ignite()
-        .mount("/", StaticFiles::from("/server/public"))
-        .launch();
+    rocket::ignite().mount("/", routes![files, index]).launch();
 }
