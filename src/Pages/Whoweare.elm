4c98ef78605a05b124e52ef2945a5f54b5b6e694
13r0ck <bnr@tuta.io>
2021-05-13 15:03:48 -0600

    Feat: Finish mainText animation + Feat: add subtext sections with animation + Feat: Add end of page buttons. Tweak: Make Home heead fill screen and make point_down a hovering box

--- src/Pages/Whoweare.elm
+++ src/Pages/Whoweare.elm
@@ -1,5 +1,8 @@
 module Pages.Whoweare exposing (Model, Msg, page)
 
+import Browser.Dom exposing (Viewport)
+import Debug exposing (toString)
+import Dict exposing (Dict)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
@@ -12,14 +15,16 @@ import Html exposing (br, div, iframe)
 import Html.Attributes exposing (attribute, class, id, property, src, style)
 import Json.Encode as Encode
 import Page
-import Palette exposing (FontSize(..), black, fontSize, gciBlue, maxWidth, white)
-import Ports exposing (disableScrolling)
+import Pages.Home_ exposing (AnimationState, When(..), onScreenItemtoCmd, updateElement)
+import Palette exposing (FontSize(..), black, fontSize, gciBlue, maxWidth, warning, white)
+import Ports exposing (disableScrolling, recvScroll)
 import Request
-import Shared exposing (Temp, ael, contactUs, footer, navbar)
+import Shared exposing (Temp, acol, ael, contactUs, footer, navbar)
 import Simple.Animation as Animation exposing (Animation)
 import Simple.Animation.Animated as Animated
 import Simple.Animation.Property as P
 import Storage exposing (Storage)
+import Task
 import View exposing (View)
 
 
@@ -40,6 +45,16 @@ page shared req =
 type alias Model =
     { showVimeo : Bool
     , simpleBtnHoverTracker : List SimpleBtn
+    , animationTracker : Dict String AnimationState
+    , subTexts : List SubText
+    }
+
+
+type alias SubText =
+    { id : Int
+    , title : String
+    , image : String
+    , text : String
     }
 
 
@@ -48,14 +63,32 @@ type alias SimpleBtn =
     , name : String
     , link : String
     , hovered : Bool
-    , message : Msg
+    , message : Maybe Msg
     }
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { showVimeo = False
-      , simpleBtnHoverTracker = [ SimpleBtn 0 "Play" "#" False OpenVimeo ]
+      , simpleBtnHoverTracker =
+            [ SimpleBtn 0 "Play" "#" False (Just OpenVimeo)
+            , SimpleBtn 1 "What we do" "/#whatwedo" False Nothing
+            , SimpleBtn 2 "Contact Us" "" False (Just OpenContactUs)
+            , SimpleBtn 3 "Technical Papers" "/technical" False Nothing
+            ]
+      , animationTracker =
+            Dict.fromList
+                [ ( "mainText", AnimationState (PercentOfViewport 40) False )
+                , ( "bottomButtons", AnimationState (PercentOfViewport 40) False )
+                , ( "1", AnimationState (PercentOfViewport 20) False )
+                , ( "2", AnimationState (PercentOfViewport 40) False )
+                , ( "3", AnimationState (PercentOfViewport 40) False )
+                ]
+      , subTexts =
+            [ SubText 1 "Sub text" "/img/subtext1.jpg" "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut."
+            , SubText 2 "Sub text" "/img/subtext2.jpg" "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut."
+            , SubText 3 "Sub text" "/img/subtext3.jpg" "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut."
+            ]
       }
     , Cmd.none
     )
@@ -73,6 +106,9 @@ type Msg
     | CloseVimeo
     | SimpleBtnHover Int
     | SimpleBtnUnHover Int
+    | Scrolled Int
+    | GotElement String (Result Browser.Dom.Error Browser.Dom.Element)
+    | OpenContactUs
 
 
 update : Storage -> Msg -> Model -> ( Model, Cmd Msg )
@@ -99,6 +135,23 @@ update storage msg model =
         SimpleBtnUnHover id ->
             ( { model | simpleBtnHoverTracker = List.indexedMap (setUnHovered id) model.simpleBtnHoverTracker }, Cmd.none )
 
+        Scrolled _ ->
+            ( model
+            , Cmd.batch
+                (List.map animationTrackerToCmd (List.filter (\( _, v ) -> v.shouldAnimate == False) (Dict.toList model.animationTracker)))
+            )
+
+        GotElement id element ->
+            case element of
+                Ok e ->
+                    ( { model | animationTracker = Dict.fromList (List.map (updateElement id e) (Dict.toList model.animationTracker)) }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+        OpenContactUs ->
+            ( model, Storage.setContactUs "True" storage )
+
 
 
 -- SUBSCRIPTIONS
@@ -106,7 +159,7 @@ update storage msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    recvScroll Scrolled
 
 
 
@@ -127,6 +180,69 @@ view shared model =
 
         isPhone =
             device == Phone
+
+        isTablet =
+            device == Tablet
+
+        isMobile =
+            isPhone || isTablet
+
+        subtext item =
+            let
+                img =
+                    el
+                        [ width (fillPortion 2)
+                        , clip
+                        , centerY
+                        , Border.rounded 10
+                        , inFront
+                            (el
+                                [ width fill
+                                , height fill
+                                , Border.innerShadow { blur = 18, color = rgba 0 0 0 0.3, offset = ( 1, 8 ), size = 8 }
+                                ]
+                                none
+                            )
+                        ]
+                        (image
+                            [ centerX
+                            , centerY
+                            , width fill
+                            ]
+                            { src = item.image, description = item.title }
+                        )
+
+                content =
+                    paragraph [ width fill, fontSize device Sm ] [ text item.text ]
+            in
+            acol
+                (if shouldAnimate (toString item.id) model then
+                    Animation.fromTo
+                        { duration = 500
+                        , options = []
+                        }
+                        [ P.opacity 0, P.y 100 ]
+                        [ P.opacity 100, P.y 0 ]
+
+                 else
+                    Animation.empty
+                )
+                [ width fill, height fill, spacing 20, htmlAttribute <| id (toString item.id), transparent (not (shouldAnimate (toString item.id) model)) ]
+                [ el [ Region.heading 3, Font.extraLight, fontSize device Lg ] (text item.title)
+                , (if isMobile then
+                    column
+
+                   else
+                    row
+                  )
+                    [ width fill, spacing 20 ]
+                    (if modBy 2 item.id == 0 || isMobile then
+                        [ img, content ]
+
+                     else
+                        [ content, img ]
+                    )
+                ]
     in
     { title = "GCI - Authorized Reverse Engineering IC Solutions for Obsolescence and High Temperature Environments"
     , attributes =
@@ -148,7 +264,7 @@ view shared model =
         ]
     , element =
         column [ width fill, Region.mainContent, clip ]
-            [ column [ width (fill |> maximum maxWidth), centerX, spacing 100 ]
+            [ column [ width (fill |> maximum maxWidth), centerX, spacing 50 ]
                 [ head shared model
                 , column
                     [ paddingXY
@@ -159,8 +275,13 @@ view shared model =
                             toFloat w * 0.2 |> round
                         )
                         0
+                    , width (fill |> maximum maxWidth)
+                    , spacing 100
                     ]
-                    [ mainText shared.temp ]
+                    ([ mainText shared.temp (shouldAnimate "mainText" model) ]
+                        ++ List.map subtext model.subTexts
+                    )
+                , bottomButtons shared (List.filter (\b -> b.id > 0) model.simpleBtnHoverTracker) (shouldAnimate "bottomButtons" model)
                 ]
             , footer shared Footer
             ]
@@ -324,16 +445,20 @@ vimeo shared =
         , height fill
         , htmlAttribute <| class "point_enter_down_long"
         , behindContent
-            (el
-                [ width fill
-                , height fill
-                , Background.gradient
-                    { angle = degrees 165
-                    , steps = [ rgba255 87 83 78 0.7, rgba255 17 24 39 0.9 ]
-                    }
-                , Events.onClick CloseVimeo
-                ]
-                none
+            (link [ width fill, height fill ]
+                { url = "/whoweare#mainText"
+                , label =
+                    el
+                        [ width fill
+                        , height fill
+                        , Background.gradient
+                            { angle = degrees 165
+                            , steps = [ rgba255 87 83 78 0.7, rgba255 17 24 39 0.9 ]
+                            }
+                        , Events.onClick CloseVimeo
+                        ]
+                        none
+                }
             )
         ]
         (column
@@ -368,8 +493,8 @@ vimeo shared =
         )
 
 
-mainText : Temp -> Element Msg
-mainText temp =
+mainText : Temp -> Bool -> Element Msg
+mainText temp animateSelf =
     let
         device =
             temp.device.class
@@ -386,18 +511,143 @@ mainText temp =
         , padding 25
         , width fill
         , inFront
-            (el [ htmlAttribute <| class "cornerClipTopLeft", width fill, height fill, Background.color black ]
-                --, inFront (el [htmlAttribute <| class "cornerClipBottomRight", width fill, height fill, Background.color black]
+            (el [ htmlAttribute <| class "clip_top", width fill, height fill, Background.color gciBlue ]
+                none
+            )
+        , inFront
+            (el
+                [ htmlAttribute <|
+                    class
+                        (if animateSelf then
+                            "animate_clip_bottom"
+
+                         else
+                            "clip_bottom"
+                        )
+                , width fill
+                , height fill
+                , Background.color gciBlue
+                ]
+                none
+            )
+        , inFront
+            (el
+                [ htmlAttribute <|
+                    class
+                        (if animateSelf then
+                            "animate_clip_cover"
+
+                         else
+                            "clip_cover"
+                        )
+                , width fill
+                , height fill
+                , Background.color white
+                ]
                 none
             )
         ]
         [ paragraph [ Font.extraLight, Region.heading 1, fontSize device Xlg ] [ text "Heading 1" ]
-        , paragraph [ spacing 10, fontSize device Sm, Font.light ]
+        , paragraph [ spacing 10, fontSize device Sm, Font.light, htmlAttribute <| id "mainText" ]
             [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit cursus risus at ultrices mi tempus imperdiet. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae."
             , html <| br [] []
             , html <| br [] []
             , text "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque elit ullamcorper dignissim cras. Et netus et malesuada fames ac turpis egestas integer."
             ]
+        ]
+
+
+bottomButtons : Shared.Model -> List SimpleBtn -> Bool -> Element Msg
+bottomButtons shared btns animateSelf =
+    let
+        h =
+            shared.temp.height
+
+        w =
+            shared.temp.width
+
+        device =
+            shared.temp.device.class
+
+        isPhone =
+            device == Phone
+
+        isTablet =
+            device == Tablet
+
+        isMobile =
+            isPhone || isTablet
+
+        btn item =
+            let
+                attr =
+                    [ centerX
+                    , Border.width 5
+                    , paddingXY 20 10
+                    , Border.rounded 10
+                    , fontSize device Md
+                    , Font.color gciBlue
+                    , Font.bold
+                    , htmlAttribute <| class "background_transition"
+                    , Border.color gciBlue
+                    , inFront
+                        (el
+                            [ htmlAttribute <|
+                                class
+                                    (if item.hovered then
+                                        "point_enter_down"
+
+                                     else
+                                        "point_leave_up"
+                                    )
+                            , centerX
+                            , centerY
+                            , paddingXY 22 10
+                            , fontSize device Md
+                            , Font.color white
+                            , Border.rounded 5
+                            , Font.bold
+                            , Background.color gciBlue
+                            ]
+                            (text item.name)
+                        )
+                    , Events.onMouseEnter (SimpleBtnHover item.id)
+                    , Events.onMouseLeave (SimpleBtnUnHover item.id)
+                    , pointer
+                    , htmlAttribute <| class "gciBtn"
+                    ]
+            in
+            case item.message of
+                Just m ->
+                    el
+                        (Events.onClick m :: attr)
+                        (text item.name)
+
+                Nothing ->
+                    link [] { url = item.link, label = el attr (text item.name) }
+    in
+    acol
+        (if animateSelf then
+            Animation.fromTo
+                { duration = 500
+                , options = []
+                }
+                [ P.opacity 0, P.y 100 ]
+                [ P.opacity 100, P.y 0 ]
+
+         else
+            Animation.empty
+        )
+        [ width fill, padding 50, spacing 10, transparent (not animateSelf), htmlAttribute <| id "bottomButtons" ]
+        [ paragraph [ Region.heading 4, fontSize device Lg, Font.center, Font.extraLight ] [ text "Want to know more about us?" ]
+        , (if isMobile then
+            column
+
+           else
+            row
+          )
+            [ centerX, spacing 10 ]
+            (List.map btn btns)
         ]
 
 
@@ -413,3 +663,18 @@ setHovered id i data =
 setUnHovered : Int -> Int -> { a | hovered : Bool } -> { a | hovered : Bool }
 setUnHovered _ _ data =
     { data | hovered = False }
+
+
+animationTrackerToCmd : ( String, AnimationState ) -> Cmd Msg
+animationTrackerToCmd ( k, _ ) =
+    Task.attempt (GotElement k) (Browser.Dom.getElement k)
+
+
+shouldAnimate : String -> Model -> Bool
+shouldAnimate id model =
+    case Dict.get id model.animationTracker of
+        Just state ->
+            state.shouldAnimate
+
+        Nothing ->
+            False
